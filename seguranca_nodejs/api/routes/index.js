ed58e2534d9e72284d36d9839a70fa0487e8cb82
Rafael Souza <rafael_popo@hotmail.com>
2025-10-16 21:31:58 -0300

    Implementando autenticacao nas APIS

--- seguranca_nodejs/api/routes/index.js
+++ seguranca_nodejs/api/routes/index.js
@@ -2,10 +2,12 @@ const bodyParser = require('body-parser')
  
 const produto = require('./produtoRoute')
 const usuario = require('./usuariosRoute')
+const auth = require('./authRoute')
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
+    auth,
     produto,
     usuario
   )
