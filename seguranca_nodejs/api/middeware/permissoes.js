4aa515a3e886255ec404359fdab5e69753efcccb
Rafael Souza <rafael_popo@hotmail.com>
2025-10-17 11:52:12 -0300

    Middleware de permissoes

---
+++ seguranca_nodejs/api/middeware/permissoes.js
@@ -0,0 +1,36 @@
+const database = require('../models')
+
+const permissoes = (listaPermissoes) => {
+    return async (req, res, next) => {
+        const { usuarioId }= req
+
+        const usuario = await database.usuarios.findOne({
+            include: [
+                {
+                    model: database.permissoes,
+                    as: 'usuario_permissoes',
+                    attributes: ['id', 'nome']
+                }
+            ],
+            where: {
+                id: usuarioId
+            }
+        })
+
+        if(!usuario){
+            return res.status(401).send('Usuário não cadastrado!')
+        }
+
+        const permissoesCadatradas =  usuario.usuario_permissoes
+            .map((permissao) => permissao.nome)
+            .some((permissao) => listaPermissoes.includes(permissao))
+
+        if(!permissoesCadatradas){
+            return res.status(401).send('Usuário não possui acesso a essa rota')
+        }
+
+        return next()
+    }
+}
+
+module.exports = permissoes
---
+++ seguranca_nodejs/api/middeware/permissoesRoles.js
@@ -0,0 +1,63 @@
+const database = require('../models')
+const Sequelize =  require('sequelize')
+
+const permissoesRoeles = (listaPermissoes) => {
+    return async (req, res, next) => {
+        const { usuarioId } = req
+        const usuario = await database.usuarios.findOne({
+            include: [
+                {
+                    model: database.roles,
+                    as: 'usuario_roles',
+                    attributes: ['id', 'nome']
+                }
+            ],
+            where: {
+                id: usuarioId
+            }
+        })
+
+        if(!usuario){
+            return res.status(401).send('Usuário não cadastrado')
+        }
+
+        let listaRolesId = []
+
+        Object.values(usuario.usuario_roles).map((role) => {
+            listaRolesId.push(role.id)
+        })
+
+        if(listaRolesId.length == 0) {
+            return res.status(401).send('Usuário não possui acesso a essa rota')
+        }
+
+        const roles = await database.roles.findAll({
+            include: [
+                {
+                    model: database.permissoes,
+                    as: 'roles_das_permissoes',
+                    attributes: ['id', 'nome']
+                }
+            ],
+            where: {
+                id: {
+                    [Sequelize.Op.in]: listaRolesId
+                }
+            }
+        })
+
+        let possuiPermissao = false;
+
+        roles.map((role) => {
+            possuiPermissao = role.roles_das_permissoes
+                .map((permissao) => permissao.nome)
+                .some((permissao) => listaPermissoes.includes(permissao))
+        })
+
+        if(!possuiPermissao){
+            return res.status(401).send('Usuário não tem acesso a essa rota!')
+        }
+
+        return next()
+    }
+}
