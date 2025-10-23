25e1aee6275f6229120604d66dd1f19e4cb84c8f
Rafael Souza <rafael_popo@hotmail.com>
2025-10-23 00:17:04 -0300

    Login e DB

--- node-auth-system/controllers/index.js
+++ node-auth-system/controllers/index.js
@@ -1,3 +1,5 @@
+const User = require('../models/user')
+
 exports.showIndex = (req, res, next) => {
     res.render('index')
 }
@@ -12,4 +14,32 @@ exports.showMembersPage = (req, res) => {
 
 exports.get404Page = (req, res, next) => {
     res.status(404).render('404')
+}
+
+exports.signup = async (req, res, next) => {
+    const {username, email, password} = req.body
+    //console.log(username, email, password)
+    const user = new User(username, email, password)
+    try {
+        await user.save()
+        res.redirect('/')
+    } catch (error) {
+        console.log(error)
+        res.redirect('signup')
+    }
+}
+
+exports.login = async(req, res, next) => {
+    const {email, password} = req.body
+    const user = await User.findOne(email, password)
+    try {
+        if (user) {
+            res.redirect('/members')
+        } else {
+            res.render('index')
+        }
+    } catch (err) {
+        console.log(err)
+        res.render('index')
+    }
 }
---
+++ node-auth-system/models/user.js
@@ -0,0 +1,22 @@
+const getDb = require('../util/database').getDb
+
+class User {
+    constructor(username, email, password) {
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+
+    async save() {
+        const db = getDb()
+        return db.collection('users').insertOne(this)
+    }
+
+    static async findOne(email, senha){
+        const db = getDb()
+        const user = await db.collection('users').findOne({email: email, password: password})
+        return user
+    }
+}
+
+module.exports = User
