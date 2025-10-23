7a62e9fc632b5d058f3586865483a7edf24c9232
Rafael Souza <rafael_popo@hotmail.com>
2025-10-23 17:59:37 -0300

    Finalizando branch feature/note-auth-system

--- node-auth-system/routes/routes.js
+++ node-auth-system/routes/routes.js
@@ -28,7 +28,6 @@ router.get('/logout', controller.logout)
 router.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
 router.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),
 function(req, res) {
-    // Successful authentication, redirect home.
     res.redirect('/members');
 });
 
